80f0c7a3761229fad387ee4d3cffca7e8794a1af
regisBafutwabo <regiswareja@gmail.com>
2022-11-15 22:19:51 +0900

    configured Helmet

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,15 @@
 import "./App.css";
 
-import logo from "../assets/images/logo.svg";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <title>퀴즈</title>
+      </Helmet>
+      <div className="App"></div>
+    </>
   );
 }
 
